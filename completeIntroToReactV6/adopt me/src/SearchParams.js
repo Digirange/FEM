c9a420fa7ab47e1eef62b0e817a1e4264e72db3a
Digirange <jarques0123@gmail.com>
2021-10-27 10:32:11 -0400

    update commit

--- completeIntroToReactV6/adopt me/src/SearchParams.js
+++ completeIntroToReactV6/adopt me/src/SearchParams.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Middletown, DE");
+  const [animal, setAnimal] = useState("");
 
   return (
     <div className="search-params">
@@ -10,10 +13,28 @@ const SearchParams = () => {
           Location:
           <input
             id="location"
-            onChange = { (e) => setLocation(e.target.value)}
+            onChange={(e) => setLocation(e.target.value)}
             value={location.toUpperCase()}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal:
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {
+              ANIMALS.map(animal => {
+                return <option value={animal} key={animal}>
+                  {animal}
+                </option>
+              })
+            }
+          </select>
         </label>
         <button>Submit</button>
       </form>
