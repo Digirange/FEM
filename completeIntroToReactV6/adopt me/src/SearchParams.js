58c014509d1ceaf1a0f9ead04c5d26cd3b890b74
Digirange <jarques0123@gmail.com>
2021-10-28 16:29:51 -0400

    Almost finished Adpot me. Will resume tomorrow

--- completeIntroToReactV6/adopt me/src/SearchParams.js
+++ completeIntroToReactV6/adopt me/src/SearchParams.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import ThemeContext from "./ThemeContext";
 import useBreedList from "./useBreedList";
-import Results from "./Results"
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -10,6 +11,7 @@ const SearchParams = () => {
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   useEffect(() => {
     requestPets();
@@ -27,10 +29,11 @@ const SearchParams = () => {
   return (
     <div className="search-params">
       <form
-        onSubmit= { (e) => {
-        e.preventDefault()
-          requestPets()
-        }}>
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location:
           <input
@@ -76,7 +79,20 @@ const SearchParams = () => {
             })}
           </select>
         </label>
-        <button>Submit</button>
+        <label htmlFor="theme">
+         Theme:
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlue={(e) => setTheme(e.target.value)}
+          >
+            <option value="darkblue">Dark Blue</option>
+            <option value="green">Green</option>
+            <option value="red">Red</option>
+            <option value="pink">Pink</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Search Animal</button>
       </form>
       <Results pets={pets} />
     </div>
