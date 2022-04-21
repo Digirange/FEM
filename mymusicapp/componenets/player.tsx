e501ceb9711e36ea4ef65c5d76d8329e4a626e25
Digirange <jarques0123@gmail.com>
2022-04-21 17:14:17 -0400

    Before Deployment chapter commit

--- mymusicapp/componenets/player.tsx
+++ mymusicapp/componenets/player.tsx
@@ -9,9 +9,9 @@ import {
   Center,
   Flex,
   Text,
-} from "@chakra-ui/react";
-import ReactHowler from "react-howler";
-import { useState, useEffect, useRef } from "react";
+} from "@chakra-ui/react"
+import ReactHowler from "react-howler"
+import { useState, useEffect, useRef } from "react"
 import {
   MdShuffle,
   MdSkipPrevious,
@@ -19,32 +19,116 @@ import {
   MdOutlinePlayCircleFilled,
   MdOutlinePauseCircleFilled,
   MdOutlineRepeat,
-} from "react-icons/md";
-import { useStoreActions } from "easy-peasy";
+} from "react-icons/md"
+import { useStoreActions } from "easy-peasy"
+import { formatTime } from "../lib/formatters"
 
 const Player = ({ songs, activeSong }) => {
-  const [playing, setPlaying] = useState(true);
-  const [index, setIndex] = useState(0);
-  const [seek, setSeek] = useState(0.0);
-  const [repeat, setReapeat] = useState(false);
-  const [shuffle, setShuffle] = useState(false);
-  const [duration, setDuration] = useState(0.0);
+  const [playing, setPlaying] = useState(true)
+  const [index, setIndex] = useState(
+    songs.findIndex((s) => s.id === activeSong.id)
+  )
+  const [seek, setSeek] = useState(0.0)
+  const [isSeeking, setIsSeeking] = useState(false)
+  const [repeat, SetRepeat] = useState(false)
+  const [shuffle, setShuffle] = useState(false)
+  const [volume, setVolume] = useState(1.0)
+  const [duration, setDuration] = useState(0.0)
+  const soundRef = useRef(null)
+  const repeatRef = useRef(repeat)
+  const setActiveSong = useStoreActions((state: any) => state.changeActiveSong)
+
+  useEffect(() => {
+    let timerId
+
+    if (playing && !isSeeking) {
+      const f = () => {
+        setSeek(soundRef.current.seek())
+        timerId = requestAnimationFrame(f)
+      }
+
+      timerId = requestAnimationFrame(f)
+      return () => cancelAnimationFrame(timerId)
+    }
+
+    cancelAnimationFrame(timerId)
+  }, [playing, isSeeking])
+
+  useEffect(() => {
+    setActiveSong(songs[index])
+  }, [index, setActiveSong, songs])
+
+  useEffect(() => {
+    repeatRef.current = repeat
+  }, [repeat])
 
   const setPlayState = (value) => {
-    setPlaying(value);
-  };
+    setPlaying(value)
+  }
 
-  const onShuffle = (value) => {
-    setShuffle((state) => !state);
-  };
+  const onShuffle = () => {
+    setShuffle((state) => !state)
+  }
 
-  const onRepeat = (value) => {
-    setReapeat((state) => !state);
-  };
+  const onRepeat = () => {
+    SetRepeat((state) => !state)
+  }
+
+  const prevSong = () => {
+    setIndex((state) => {
+      return state ? state - 1 : songs.length - 1
+    })
+  }
+
+  const nextSong = () => {
+    setIndex((state) => {
+      if (shuffle) {
+        const next = Math.floor(Math.random() * songs.length)
+
+        if (next === state) {
+          return nextSong()
+        }
+        return next
+      }
+      return state === songs.length - 1 ? 0 : state + 1
+    })
+  }
+
+  const onEnd = () => {
+    if (repeatRef.current) {
+      setSeek(0)
+      soundRef.current.seek(0)
+    } else {
+      nextSong()
+    }
+  }
+
+  const onLoad = () => {
+    const songDuration = soundRef.current.duration()
+    setDuration(songDuration)
+  }
+
+  const onSeek = (e) => {
+    setSeek(parseFloat(e[0]))
+    soundRef.current.seek(e[0])
+  }
+
+  const onVolumeChange = () => {
+    setVolume
+  }
 
   return (
     <Box>
-      <Box>{/* <ReactHowler playing={playing} src={activeSong?.url} /> */}</Box>
+      <Box>
+        <ReactHowler
+          playing={playing}
+          src={activeSong?.url}
+          ref={soundRef}
+          onLoad={onLoad}
+          onEnd={onEnd}
+          // volume={}
+        />
+      </Box>
       <Center color="gray.600">
         <ButtonGroup>
           <IconButton
@@ -61,6 +145,7 @@ const Player = ({ songs, activeSong }) => {
             variant="link"
             aria-label="previous"
             fontSize="24px"
+            onClick={prevSong}
             icon={<MdSkipPrevious />}
           />
           {playing ? (
@@ -91,6 +176,7 @@ const Player = ({ songs, activeSong }) => {
             aria-label="next"
             fontSize="24px"
             icon={<MdSkipNext />}
+            onClick={nextSong}
           />
           <IconButton
             outline="none"
@@ -107,14 +193,18 @@ const Player = ({ songs, activeSong }) => {
       <Box color="gray.600">
         <Flex justify="center" align="center">
           <Box width="10%">
-            <Text fontSize="x-small">1:21</Text>
+            <Text fontSize="x-small">{formatTime(duration)}</Text>
           </Box>
           <Box width="80%">
             <RangeSlider
               aria-label={["min", "max"]}
               step={0.1}
               min={0}
-              max={321}
+              max={duration ? duration.toFixed(2) : 0}
+              onChange={onSeek}
+              value={[seek]}
+              onChangeStart={() => setIsSeeking(true)}
+              onChangeEnd={() => setIsSeeking(false)}
               id="player-range"
             >
               <RangeSliderTrack bg="gray.800">
@@ -124,12 +214,12 @@ const Player = ({ songs, activeSong }) => {
             </RangeSlider>
           </Box>
           <Box width="10%" textAlign="right">
-            <Text fontSize="x-small">3:21</Text>
+            <Text fontSize="x-small">{formatTime(duration)}</Text>
           </Box>
         </Flex>
       </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default Player;
+export default Player
--- mymusicapp/componenets/playerBar.tsx
+++ mymusicapp/componenets/playerBar.tsx
@@ -1,20 +1,42 @@
-import { Box, Flex, Text } from "@chakra-ui/layout";
-import Player from "./player";
+import { Box, Flex, Text } from "@chakra-ui/layout"
+import { RangeSlider, RangeSliderTrack, RangeSliderFilledTrack, RangeSliderThumb } from "@chakra-ui/react"
+import { useStoreState } from "easy-peasy"
+import Player from "./player"
 
 const PlayerBar = () => {
+  const songs = useStoreState((state: any) => state.activeSongs)
+  const activeSong = useStoreState((state: any) => state.activeSong)
   return (
     <Box height="100px" width="100vw" bg="gray.900" padding="10px">
       <Flex align="center">
-        <Box padding="20px" color="white" width="30%">
-          <Text fontSize="large">Song Name</Text>
-          <Text fontSize="sm">Artist name</Text>
-        </Box>
+        {activeSong ? (
+          <Box padding="20px" color="white" width="30%">
+            <Text fontSize="large">{activeSong.name}</Text>
+            <Text fontSize="sm">{activeSong.artist.name}</Text>
+          </Box>
+        ) : null}
+
         <Box width="40%">
-          <Player/>
+          {activeSong ? <Player songs={songs} activeSong={activeSong} /> : null}
         </Box>
+        <Box width="30%" alignContent="right">
+          <Flex>
+            <Box width="15%">
+              <RangeSlider
+              aria-label={["min", "max"]}
+              defaultValue={[1.0]}
+            >
+              <RangeSliderTrack>
+                <RangeSliderFilledTrack/>
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+              </RangeSlider>
+            </Box>
+          </Flex>  
+          </Box>
       </Flex>
     </Box>
-  );
-};
+  )
+}
 
-export default PlayerBar;
+export default PlayerBar
